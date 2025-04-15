a203529241fcedde62c3e55b94e675fe7642b68f
Jo3-h <joe.hosking1@gmail.com>
2025-04-15 17:44:45 +0930

    continuing with beset_split method

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -6,6 +6,12 @@ import pandas as pd
 '''
 class TreeNode:
 
+    # constructor used for initialising a leaf node
+    def __init__(self, rows):
+        self.rows = rows
+        self.leaf = True
+
+    # constructor used for initialising a decision node
     def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
         self.feature = feature
         self.threshold = threshold
@@ -13,9 +19,7 @@ class TreeNode:
         self.right = right
         self.value = value
         self.data_points = 0
-
-    def is_leaf(self):
-        return self.value is not None
+        self.leaf = False
 
 '''
 -----> Decision Tree Classifier <-----
@@ -35,7 +39,7 @@ class DecisionTree:
         self.criterion = criterion
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
-        self.min_samples_leaef = min_samples_leaf
+        self.min_samples_leaf = min_samples_leaf
         self.max_leaf_nodes = max_leaf_nodes
         self.root = None
         return
@@ -48,14 +52,71 @@ class DecisionTree:
     def build_tree(self, data, depth=0):
 
         # base case if no data is passed
-        if len(data) == 0:
+        if len(data) == 0 or depth > self.max_depth:
             return None
-        
-        # if depth exceeds max_depth parameter this node must be leaf
-        if depth > self.max_depth:
-            return
+
+        # calculate the information gain, split feature, and threshold value
+        info_gain, feature, threshold = _find_best_split(rows)
+
+        if info_gain == 0:
+            return TreeNode(rows)
 
         return
+
+    # Method for finding the most information gain in a candidate split on feature at threshold
+    def _find_best_split(rows):
+
+
+
+        best_gain = 0
+        best_feature = None
+        feature_count = len(rows[0]) - 1
+
+        for feature in range(feature_count):
+
+            candidate_thresholds, categorical = self._get_candidates(rows, feature)
+
+            for candidate in candidate_thresholds:
+
+                true_rows, false_rows = self._partition(rows, feature, candidate, categorical)
+
+                if len(true_rows) 
+
+    def _partition(rows, feature, candidate, categorical):
+        
+        true_rows, false_rows = [], []
+        for row in rows:
+
+            # partition based on categorical feature
+            if categorical:
+                if row[feature] == candidate:
+                    true_rows.append(row)
+                else:
+                    false_rows.append(row)
+
+            else:
+                if row[feature] >= candidate:
+                    true_rows.append(row)
+                else:
+                    false_rows.append(row)
+        
+        return true_rows, false_rows
+
+
+    def _get_candidates(rows, feature):
+
+        if type(rows[0][feature]) == str: # Categorical feature
+            return set(row[feature] for row in rows), True
+
+        else: # Continuous feature
+            sorted_elements = sorted(set(row[feature] for row in rows))
+            candidates = []
+            for i in range(len(sorted_elements) - 1):
+                midpoint = (sorted_elements[i] + sorted_elements[i+1])/2
+                candidates.append(midpoint)
+
+            return candidates, False
+
 
     # Method to calculate the Gini Impurity of a given set of rows
     def _gini_impurity(self, rows):
